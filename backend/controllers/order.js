2603c0d9164586e6e042677e6c4af5b5f5943b0a
MasterMobo <mastermobo101@gmail.com>
2023-05-11 21:19:18 +0700

    Update order.js

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,24 +1,23 @@
-const order = require("../models/order");
-
+const Order = require("../models/order");
 
 const getOrders = async (req, res) => {
-    const orders = await order.find({});
+    const orders = await Order.find({});
     res.status(200).json({ orders });
-}
+};
 
 const getOrderId = async (req, res) => {
-    const order = await order.findById(req.params.id);
+    const order = await Order.findById(req.params.id);
     res.status(200).json({ order });
-}
+};
 
 const addOrder = async (req, res) => {
-    const order = await order.create(req.body);
+    const order = await Order.create(req.body);
     res.status(201).json({ order });
-}
+};
 
 const deleteOrder = async (req, res) => {
-    const order = await order.findByIdAndDelete(req.params.id);
+    const order = await Order.findByIdAndDelete(req.params.id);
     res.status(200).json({ order });
-}
+};
 
-module.exports = {getOrders, getOrderId, addOrder, deleteOrder}
+module.exports = { getOrders, getOrderId, addOrder, deleteOrder };
