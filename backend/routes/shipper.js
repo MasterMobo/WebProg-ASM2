3dda04c4ebc453ae7a372ad43d55c28d58cce52f
MasterMobo <mastermobo101@gmail.com>
2023-05-15 13:26:09 +0700

    Update shipper.js

--- backend/routes/shipper.js
+++ backend/routes/shipper.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getShippers, getShipperId, updateOrder } = require("../controllers/shipper");
+const {
+    getShippers,
+    getShipperId,
+    updateOrder,
+} = require("../controllers/shipper");
 
 router.route("/").get(getShippers);
-router.route("/:id").get(getShipperId);
 router.route("/order/:id").patch(updateOrder);
+router.route("/:id").get(getShipperId);
 
 module.exports = router;
