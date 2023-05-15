50f0b97fa25608e01a2209db426ba168abe02029
MasterMobo <mastermobo101@gmail.com>
2023-05-15 14:25:55 +0700

    added product search and filter

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,18 +1,31 @@
 const Product = require("../models/product");
-const {NotFoundError, UnauthorizedError} = require("../errors/index");
+const { NotFoundError, UnauthorizedError } = require("../errors/index");
 
 const getProducts = async (req, res) => {
-    const products = await Product.find({});
+    const { name, minPrice, maxPrice } = req.query;
+
+    let query = {};
+    if (name) {
+        query.name = { $regex: name, $options: "i" };
+    }
+    if (minPrice) {
+        query.price = { $gte: minPrice };
+    }
+    if (maxPrice) {
+        query.price = { ...query.price, $lte: maxPrice };
+    }
+    console.log(query);
+    const products = await Product.find(query);
     return res.status(200).json({ products });
-}
+};
 
 const getProductId = async (req, res) => {
     const product = await Product.findById(req.params.id);
-    
+
     if (!product) {
         throw new NotFoundError("Product not found");
     }
     return res.status(200).json({ product });
-}
+};
 
-module.exports = {getProducts, getProductId}
+module.exports = { getProducts, getProductId };
