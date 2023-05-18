944aa5d605563db19a60c01318bc3b0e60d04153
Alakinnn <117524098+Alakinnn@users.noreply.github.com>
2023-05-19 01:37:12 +0700

    semi structure update

--- frontend/header/header.js
+++ frontend/header/header.js
@@ -1,11 +1,17 @@
 const hamburger_menu = () => {
-    let menu = document.querySelector("#menu-icon");
-        let navbar = document.querySelector("#responsive_nav");
-        menu.onclick = () =>{
-            menu.classList.toggle("bx-x");
-            navbar.classList.toggle("close");
-            navbar.classList.toggle("open");
-        }
+  let menu = document.querySelector("#menu-icon");
+  let navbar = document.querySelector("#responsive_nav");
+  menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    navbar.classList.toggle("close");
+    navbar.classList.toggle("open");
+  };
+};
+
+function redirectFunction() {
+  location.href("https://www.tutorialspoint.com/index.htm");
 }
 
-hamburger_menu()
+document.querySelector(".btn-log").addEventListener("click", redirectFunction);
+
+hamburger_menu();
