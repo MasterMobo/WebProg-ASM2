d7d27fb82d92b0811a028d62a697d169975daf5d
MasterMobo <mastermobo101@gmail.com>
2023-11-30 11:25:38 +0700

    feature 4

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const start = async () => {
         console.log("feature 2");
         console.log("main 3");
         console.log("feature 3");
+        console.log("feature 4");
         console.log(`Server is running on port ${PORT}`);
     });
     console.log("Connecting to database...");
