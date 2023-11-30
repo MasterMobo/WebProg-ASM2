48617eab7f4774054fedbd5c29925f069410fddf
MasterMobo <mastermobo101@gmail.com>
2023-11-30 11:18:37 +0700

    feature 1

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const PORT = process.env.PORT || 3000;
 
 const start = async () => {
     await app.listen(PORT, () => {
-        console.log("main 1");
+        console.log("feature 1");
         console.log(`Server is running on port ${PORT}`);
     });
     console.log("Connecting to database...");
